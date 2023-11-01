d30a982261f7c847a8c4cddb026c1f72434d8e83
Martín García <mgarciara@fpcoruna.afundacion.org>
2023-11-01 23:00:16 +0100

    Eliminado archivos innecesarios y arreglado error con key.

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import { useState } from 'react';
 
 function App() {
   const [busqueda, setBusqueda] = useState("")
-  const [peliculasEncontradas, setPeliculasEncontradas ] = useState([])
+  const [peliculasEncontradas, setPeliculasEncontradas] = useState([])
   const [overview, setOverview] = useState("");
 
 
-  const cambioBusqueda = evt => { 
+  const cambioBusqueda = evt => {
     const elemento = evt.currentTarget;
     const nuevoValor = elemento.value;
     setBusqueda(nuevoValor);
@@ -21,66 +21,69 @@ function App() {
         'accept': 'application/json'
       }
     })
-    .then( response => response.json())
-    .then( data => {
-      console.log(data);
-      console.log(data.results[6].original_title);
-      console.log(data.results[6].release_date);
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        console.log(data.results[6].original_title);
+        console.log(data.results[6].release_date);
 
-      // map transformat cada delemento a otra cosa usando una funcion.
+        // map transformat cada delemento a otra cosa usando una funcion.
 
-      //const ciudades = ["san diego", "tokio", "bombay"]
-      //ciudades.map(cadena => cadena.toUpperCase())
-
-
-      let peliculas = data.results.map(cadena => ({ "original_title" : cadena.original_title,
-                                                 "release_date": cadena.release_date,
-                                                "id": cadena.id}));
-
-      setPeliculasEncontradas(peliculas)
-/*
-      const panelResultado = document.getElementById("resultado")
-      panelResultado.innerHTML = ""
-      for(let pelicula of peliculasEncontradas) {
-        panelResultado.innerHTML += <h2>pelicula.original_title</h2>
-      }
-      */
-      //peliculasEncontradas = peliculas;
-      console.log(peliculas);
-      // array -> objetos con 2 campos: original_title y el campo release_date
+        //const ciudades = ["san diego", "tokio", "bombay"]
+        //ciudades.map(cadena => cadena.toUpperCase())
 
 
+        let peliculas = data.results.map(cadena => ({
+          "original_title": cadena.original_title,
+          "release_date": cadena.release_date,
+          "id": cadena.id
+        }));
 
-    })
+        setPeliculasEncontradas(peliculas)
+        /*
+              const panelResultado = document.getElementById("resultado")
+              panelResultado.innerHTML = ""
+              for(let pelicula of peliculasEncontradas) {
+                panelResultado.innerHTML += <h2>pelicula.original_title</h2>
+              }
+              */
+        //peliculasEncontradas = peliculas;
+        console.log(peliculas);
+        // array -> objetos con 2 campos: original_title y el campo release_date
+
+
+
+      })
 
   }
 
-  const detallesPelicula = (data) =>{
+  const detallesPelicula = (data) => {
     console.log(data);
-    fetch(`https://api.themoviedb.org/3/movie/${data.id}`, {headers: {
-      'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxMzU0M2FjODg2ZTNmMmFiNzk1YmRjOGExMzY1NGE2MCIsInN1YiI6IjY1MzZlNmM0NDA4M2IzMDEzNzEzNWY3YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.yqoDP60qX3AiwObHa7-e2Kz6KL3irRWTzDPgmyPB53U',
-      'accept': 'application/json'
+    fetch(`https://api.themoviedb.org/3/movie/${data.id}`, {
+      headers: {
+        'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxMzU0M2FjODg2ZTNmMmFiNzk1YmRjOGExMzY1NGE2MCIsInN1YiI6IjY1MzZlNmM0NDA4M2IzMDEzNzEzNWY3YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.yqoDP60qX3AiwObHa7-e2Kz6KL3irRWTzDPgmyPB53U',
+        'accept': 'application/json'
       }
-    }).then( (response) => response.json() ).then((data) => {
+    }).then((response) => response.json()).then((data) => {
       console.log(data);
       setOverview(data.overview)
     })
 
-    
+
     // console.log(data.results[data.].original_title);
   }
 
-   // JSX
+  // JSX
   return (
     <div className="App">
       <input value={busqueda} onChange={cambioBusqueda} />
       <button onClick={clickBoton}>Buscar</button>
-      <hr/>
+      <hr />
       {
-        peliculasEncontradas.map(pelicula => <button onClick={e => detallesPelicula(pelicula)}>{pelicula.original_title}</button>)
+        peliculasEncontradas.map(pelicula => <button key={pelicula.id} onClick={e => detallesPelicula(pelicula)}>{pelicula.original_title}</button>)
       }
       <div className="DetallesPelicula">
-        { overview }
+        {overview}
       </div>
     </div>
   );
