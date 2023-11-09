cf3a31e4ed6e5ab853d9122f40b96ab9255d2708
Martín García <mgarciara@fpcoruna.afundacion.org>
2023-11-09 22:58:08 +0100

    Creación de Layout: He aplicado un layout para que se vea como cabecera de todas las secciones de la web.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import './App.css';
-import {Routes, BrowserRouter, Route} from "react-router-dom";
-import Search from './pages/Search';
-import Info from './pages/Info';
-
+import "./App.css";
+import { Routes, BrowserRouter, Route } from "react-router-dom";
+import Search from "./pages/Search";
+import Info from "./pages/Info";
+import MainLayout from "./layout/MainLayout";
 function App() {
   // JSX
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path='/Buscador' element={<Search/>} />
-          <Route path='/Info' element={<Info/>} />
+          <Route path="/" element={<MainLayout />}>
+            <Route path='/Buscador' element={<Search />} />
+            <Route path='/Info' element={<Info />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </div>
