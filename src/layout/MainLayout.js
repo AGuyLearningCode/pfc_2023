e8d8d84618bf387bc7af25e92e39a77b69121889
Martín García <mgarciara@fpcoruna.afundacion.org>
2023-11-09 23:55:17 +0100

    Creación del menú superior: Implementación de menú en el layout.

--- src/layout/MainLayout.js
+++ src/layout/MainLayout.js
@@ -1,14 +1,19 @@
 import React from 'react'
-import { Outlet } from 'react-router-dom'
+import { Link, Outlet } from 'react-router-dom'
 
 const MainLayout = () => {
   return (
     <div>
-        <header>
-            <h1>AVMaster</h1>
-        </header>
-        <Outlet />
-        
+      <header>
+        <h1>AVMaster</h1>
+        <nav>
+          <Link to="/Buscador"> Buscador</Link> |
+          <Link to="/Cartelera"> Cartelera</Link> |
+          <Link to="/Proximamente"> Proximamente</Link>
+        </nav>
+      </header>
+      <hr />
+      <Outlet />
     </div>
   )
 }
