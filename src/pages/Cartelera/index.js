0bf1708501e6ec864649f21afc8891c586e4b205
Martín García <mgarciara@fpcoruna.afundacion.org>
2023-11-10 21:39:24 +0100

    Cartelera: implementado el useEffect.

--- src/pages/Cartelera/index.js
+++ src/pages/Cartelera/index.js
@@ -3,24 +3,29 @@ import { getURL } from '../../helpers/fetchHelpers'
 import ItemResultadoBusqueda from '../../components/ItemResultadoBusqueda';
 import { useState } from 'react';
 import { Navigate, useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
 
 const Cartelera = () => {
 
-    const [peliculas, setPeliculas]=useState([]);
+    const [peliculas, setPeliculas] = useState([]);
     const navigate = useNavigate();
 
-    getURL("movie/now_playing").then(resultado=>{
-        setPeliculas(resultado.results);
-    });
+    useEffect(() => {
+        getURL("movie/now_playing").then(resultado => {
+            setPeliculas(resultado.results);
+        });
+    }, [])
+
+
 
 
     return (
         <div>
             <h1>Esta es la cartelera</h1>
 
-            { peliculas.map(p => <ItemResultadoBusqueda img={p.poster_path} onClick={e=>(navigate(`/Info/${p.id}`))}/>) }
+            {peliculas.map(p => <ItemResultadoBusqueda img={p.poster_path} onClick={e => (navigate(`/Info/${p.id}`))} />)}
 
-            
+
         </div>
     )
 }
