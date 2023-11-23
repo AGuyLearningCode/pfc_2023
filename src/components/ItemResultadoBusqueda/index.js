6ffa1bbaef559df0f15d99924441a432621b3b1f
Martín García <mgarciara@fpcoruna.afundacion.org>
2023-11-23 23:53:50 +0100

    Se ha implementado el título a los resultados de búsqueda.

--- src/components/ItemResultadoBusqueda/index.js
+++ src/components/ItemResultadoBusqueda/index.js
@@ -4,6 +4,7 @@ import placeholder from '../../assets/imagenes/Item-placeholder_3.png'
 const ItemResultadoBusqueda = (props) => {
   return (
     <div className={styles.resultado} onClick={ props.onClick }>
+      <div className={styles.imagenplaceholder}>
         {props.img!=null &&
         <img src={
           "https://www.themoviedb.org/t/p/w300_and_h450_bestv2"
@@ -13,11 +14,16 @@ const ItemResultadoBusqueda = (props) => {
 
 
         {props.img===null &&
-        <div className={styles.imagenplaceholder}>
+        
           <img src={placeholder}/>
-        </div>
+        
           
         }
+
+        <div className={styles.tituloPelicula}>
+          <h1>{props.title}</h1>
+        </div>
+        </div>
     </div>
   );
 }
