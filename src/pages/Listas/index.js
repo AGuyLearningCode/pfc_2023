590b54d79242e82b52df5e42703559b54ea4c6d5
Martín García <mgarciara@fpcoruna.afundacion.org>
2023-12-03 11:16:10 +0100

    Implementación del filtrado por género en el listado de series de la sección: Listas.

--- src/pages/Listas/index.js
+++ src/pages/Listas/index.js
@@ -10,11 +10,17 @@ const Listas = () => {
     const [favoritos, setFavoritos] = useState(listasManager.getFavoritos());
     const [filtrosPeliculas, setFiltrosPeliculas] = useState([])
     const [filtrosActivosPeliculas, setFiltrosActivosPeliculas] = useState([])
+    const [filtrosSeries, setFiltrosSeries] = useState([])
+    const [filtrosActivosSeries, setFiltrosActivosSeries] = useState([])
 
 
     useEffect(() => {
-        const generos = favoritos.filter(p => p.tipo === "p").flatMap((f) => f.generos).filter((g,i,a) => a.slice(0,i).map(e => e.id).indexOf(g.id) === -1)
-        setFiltrosPeliculas(generos)
+        const generosPeliculas = favoritos.filter(p => p.tipo === "p").flatMap((f) => f.generos).filter((g,i,a) => a.slice(0,i).map(e => e.id).indexOf(g.id) === -1)
+        setFiltrosPeliculas(generosPeliculas)
+
+        const generosSeries = favoritos.filter(p => p.tipo === "s").flatMap((f) => f.generos).filter((g,i,a) => a.slice(0,i).map(e => e.id).indexOf(g.id) === -1)
+        setFiltrosSeries(generosSeries)
+
     }, [favoritos])
 
     const eliminarFavorito = (favorito) => {
@@ -34,6 +40,19 @@ const Listas = () => {
         }
     }
 
+    const cambiarFiltroSeries =(id) => {
+        if(filtrosActivosSeries.includes(id)){
+            setFiltrosActivosSeries((filtrosActivos) => [...filtrosActivos].filter(e => e !== id))
+        } else {
+            setFiltrosActivosSeries((filtrosActivos) => {
+                const filtros = [...filtrosActivos];
+                filtros.push(id)
+                return filtros
+            })
+        }
+    }
+
+
     return (
         // <div className={styles.resultado}>
         <div className={styles.listas}>
@@ -47,8 +66,15 @@ const Listas = () => {
             <ul>
                 { favoritos.filter(f => f.tipo === "p").filter(p => filtrosActivosPeliculas.length === 0 || filtrosActivosPeliculas.filter(f => p.generos.map(p => p.id).includes(f)).length > 0).map(f => <li key={f.id}><img src={f.poster ? "https://www.themoviedb.org/t/p/w300_and_h450_bestv2" + f.poster : itemplaceholder} />{f.tipo}{f.nombre} <Button onClick={() => eliminarFavorito(f)}>Eliminar</Button></li> )  }
             </ul>
+
+            <div>
+                {
+                    filtrosSeries.map(f => <React.Fragment key={f.id}><input type="checkbox" value={f.id} checked={ filtrosActivosSeries.includes(f.id) }
+                        onChange={() => cambiarFiltroSeries(f.id)}></input> {f.name}</React.Fragment>)
+                }
+            </div>
             <ul>
-                { favoritos.filter(f => f.tipo === "s").map(f => <li key={f.id}><img src={f.poster ? "https://www.themoviedb.org/t/p/w300_and_h450_bestv2" + f.poster : itemplaceholder} />{f.tipo}{f.nombre} <Button onClick={() => eliminarFavorito(f)}>Eliminar</Button></li> )  }
+                { favoritos.filter(f => f.tipo === "s").filter(p => filtrosActivosSeries.length === 0 || filtrosActivosSeries.filter(f => p.generos.map(p => p.id).includes(f)).length > 0).map(f => <li key={f.id}><img src={f.poster ? "https://www.themoviedb.org/t/p/w300_and_h450_bestv2" + f.poster : itemplaceholder} />{f.tipo}{f.nombre} <Button onClick={() => eliminarFavorito(f)}>Eliminar</Button></li> )  }
             </ul>
             
         </div>
